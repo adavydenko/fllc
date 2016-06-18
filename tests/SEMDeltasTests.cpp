45f672e0f7746949e9165bbfdfccbe78a721c5f8
adavydenko <alexandrdavydenko@yandex.ru>
2016-06-18 22:55:59 +0300

    test added

--- tests/SEMDeltasTests.cpp
+++ tests/SEMDeltasTests.cpp
@@ -11,7 +11,7 @@ namespace tests
     TEST_CLASS(SEMDeltasTests)
     {
     public:
-        TEST_METHOD(BasicTest)
+        TEST_METHOD(BasicIntTest)
         {
             int x[5] = { 100, 200, 250, 260, 405 };
 
@@ -26,6 +26,26 @@ namespace tests
             for (size_t i = 0; i < 5; i++)
             {
                 Assert::AreEqual(x[i], unpacked[i], L"Invalid decompressed value.", LINE_INFO());
+            }
+
+            delete[] zip;
+        }
+
+        TEST_METHOD(BasicFloatTest)
+        {
+            float x[5] = { 100.0f, 200.0f, 250.0f, 260.0f, 405.0f };
+
+            SEMDeltas compressor;
+            compressor.compress((_float*)x, 5);
+
+            int size;
+            const unsigned char* zip = compressor.allocate(&size);
+
+            SEMDeltas decompressor;
+            float* unpacked = (float*)decompressor.decompress(zip, size, 5);
+            for (size_t i = 0; i < 5; i++)
+            {
+                Assert::AreEqual(x[i], unpacked[i], 0.000001f, L"Invalid decompressed value.", LINE_INFO());
             }
 
             delete[] zip;
